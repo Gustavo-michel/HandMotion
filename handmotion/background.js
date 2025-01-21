08b4754089485d9f3afef22b50b725c95725ba1b
Gustavo Michel <Gustavomichelads@gmail.com>
2025-01-21 20:09:33 -0300

    Background configure

--- handmotion/background.js
+++ handmotion/background.js
@@ -4,11 +4,13 @@ let isTrackingActive = false;
 async function checkServerStatus() {
     try {
         const response = await fetch(`${serverUrl}/status`);
-        if (!response.ok) throw new Error("Servidor Python não está ativo");
-        console.log("Servidor Python está ativo.");
-        return true;
+        if (response.ok) {
+            console.log("Servidor Python ativo.");
+            return true;
+        }
+        throw new Error("Falha ao acessar o servidor.");
     } catch (error) {
-        console.error("Erro ao conectar ao servidor Python:", error);
+        console.error("Erro de conexão:", error.message);
         return false;
     }
 }
